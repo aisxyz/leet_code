33d6e7549e5e763b9c729bb547f197a5734d068d
lei <leipeng1211@foxmail.com>
2016-01-18 17:18:46 +0800

    modify some errors

--- longestIncreasingSubsequence.py
+++ longestIncreasingSubsequence.py
@@ -2,30 +2,30 @@
 
 class LongestIncreasingSubsequence(object):
     def longestIncreasingSubsequence(self, nums):
-        increasingSubsequence =[]
-        positionIndex = 0
-        for biggerThanThis in nums:
-            itemList = [biggerThanThis]
-            tempList = nums[positionIndex+1:]
-            for num in tempList:
-                if num > biggerThanThis:
-                    itemList.append(num)
-                else:
-                    break
-            if len(increasingSubsequence) < len(itemList):
-                increasingSubsequence = itemList
-            if len(increasingSubsequence) == len(tempList)+1:
-                break
-            positionIndex += 1
-        if len(increasingSubsequence) == 1:
-            return increasingSubsequence
-        return (increasingSubsequence[:1] + self.longestIncreasingSubsequence(increasingSubsequence[1:]))
+		if len(nums) == 0:
+			return []
+		increasingSubsequence = []
+		positionIndex = 0
+		for biggerThanThis in nums:
+			itemList = [biggerThanThis]
+			tempList = nums[positionIndex+1:]
+			for num in tempList:
+				if num > biggerThanThis:
+					itemList.append(num)
+			if len(increasingSubsequence) < len(itemList):
+				increasingSubsequence = itemList
+			if len(increasingSubsequence) == len(tempList)+1:
+				break
+			positionIndex += 1
+		if len(increasingSubsequence) == 1:
+			return increasingSubsequence
+		return (increasingSubsequence[:1] + self.longestIncreasingSubsequence(increasingSubsequence[1:]))
         
     def lengthOfLongestIncreasingSubsequence(self, nums):
-        return len(self.longestIncreasingSubsequence(nums))
+		return len(self.longestIncreasingSubsequence(nums))
 
 if __name__ == '__main__':
 	longestIS = LongestIncreasingSubsequence()
-	testSequence = [10,9,2,5,3,3,7,101,18]
-	#print longestIS.longestIncreasingSubsequence(testSequence)
+	testSequence = []
+	print longestIS.longestIncreasingSubsequence(testSequence)
 	print longestIS.lengthOfLongestIncreasingSubsequence(testSequence)
